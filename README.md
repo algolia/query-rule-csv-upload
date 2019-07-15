1723f4c59449aa2b3d0bb15877cd2f274801d6d3
Chris Lebowitz <chris.lebowitz@algolia.com>
2019-07-15 15:29:05 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Query Rule CSV Upload
+A tool that enables users to manage their query rules via a spreadsheet 
+
+## Requirements
+* [Node.js](https://nodejs.org/)
+
+## Installation
+To run locally/in development environment:
+* Download or clone this repository
+* Navigate to the project root directory
+* Install the required components using `npm install`
+* Run `npm start` to start the Electron app 
+
+To build:
+* Download or clone this repository
+* Navigate to the project root directory
+* Install the required components using `npm install`
+* Run the [electron-builder](https://www.electron.build/cli) command appropriate for your platform
+  * For example, to build for Mac, you would run `electron-builder -m`
+  * Multiple environments can be built in parallel
---
+++ query_rules_script.js
@@ -0,0 +1,139 @@
+/**
+ * User: chrislebowitz
+ * Date: 2019-06-17
+ * Time: 09:51
+ */
+
+const algoliasearch = require('algoliasearch');
+const fs = require('fs');
+const csv = require('csv-parser');
+
+
+(function(){
+    const getInputs = (form) => {
+        const inputs = form.getElementsByTagName('input');
+        return [...inputs];
+    };
+
+    const getFields = (form) => {
+        return getInputs(form).reduce((obj, item) => {
+            obj[item.name + 'Input'] = item;
+            return obj;
+        }, {});
+    };
+
+    const isValid = (form) => {
+        const inputs = getInputs(form);
+        for(let i = 0; i < inputs.length; i++){
+            const input = inputs[i];
+            if(!input.value){
+                return false;
+            }
+        }
+        return true;
+    };
+
+    const setError = (errorContainer, text) => {
+        errorContainer.innerText = text;
+    };
+
+    const resetError = (errorContainer) => {
+        setError(errorContainer, '');
+    };
+
+    const processForm = (form, errorContainer) => {
+        const {fileInput, appIdInput, apiKeyInput, indexNameInput} = getFields(form);
+        const client = algoliasearch(appIdInput.value, apiKeyInput.value);
+        const index = client.initIndex(indexNameInput.value);
+        let rules = [];
+
+        const file = fileInput.files[0];
+        fs.createReadStream(file.path, {
+            autoclose: true,
+            flags: 'r'
+        })
+            .pipe(csv())
+            .on('data', (row) => {
+                try {
+                    let rule = {};
+
+                    const queryUpdated = row['Date Updated'].trim();
+                    const queryUpdatedBy = row['Updated By'].trim();
+                    const queryPatternID = row['Query Rule ID'].trim();
+                    const queryContext = row['Context'] && row['Context'].trim();
+                    const queryAnchoring = (row['Anchoring'] && row['Anchoring'].trim()) || 'contains';
+                    const queryPattern = row['Search Term'].trim();
+                    const queryReplacement = row['Replace Query'].trim();
+                    const queryEnabled = row['Enabled'].trim();
+
+
+                    const formattedQueryPattern = queryPatternID.replace(/\s+/g, '-');
+                    const objectID = queryContext + "--" + formattedQueryPattern;
+                    const objectDescription = `${queryPatternID} - ${queryContext} - updated ${queryUpdated} by ${queryUpdatedBy}`;
+
+                    rule.objectID = objectID;
+                    rule.description = objectDescription;
+                    rule.enabled = queryEnabled.toLowerCase() !== 'false';
+                    rule.condition = {
+                        pattern: queryPattern,
+                        anchoring: queryAnchoring
+                    };
+                    if(queryContext){
+                        rule.condition.context = queryContext;
+                    }
+
+                    let consequence = {};
+                    if(queryReplacement){
+                        consequence.params = {
+                            query: queryReplacement
+                        };
+                    }
+                    let userData = {};
+                    //TODO figure out how to make use this list above
+                    const keyExclusions = ['Date Updated', 'Updated By', 'Query Rule ID', 'Context', 'Anchoring', 'Search Term', 'Replace Query', 'Enabled'];
+                    for(let key in row){
+                        if(row.hasOwnProperty(key) && keyExclusions.indexOf(key) === -1 && typeof row[key] === 'string'){
+                            userData[key] = row[key].trim();
+                        }
+                    }
+                    consequence.userData = userData;
+
+                    rule.consequence = consequence;
+
+                    rules.push(rule);
+                }
+                catch(e){
+                    setError(e.message);
+                }
+            })
+            .on('end', () => {
+                if(rules.length > 0){
+                    index.batchRules(rules, (err) => {
+                        if(err){
+debugger;
+                            setError(errorContainer, 'Please enter a valid application ID and API key.');
+                        }
+                        else {
+                            let successMessage = document.createElement('span');
+                            successMessage.className = 'success';
+                            successMessage.innerText = 'Successfully updated ' + rules.length + ' query rules.';
+                            errorContainer.innerHTML = successMessage.outerHTML;
+                        }
+                    });
+                }
+            });
+    };
+
+    const form = document.getElementById('csv-upload-form');
+    form.addEventListener('submit', (event) => {
+        const errorContainer = document.getElementById('errors');
+        if(!isValid(form)){
+            setError(errorContainer, 'Please enter values in all of the fields.');
+        }
+        else {
+            resetError(errorContainer);
+            processForm(form, errorContainer);
+        }
+        event.preventDefault();
+    });
+})();
